675da59f8d45b4904bece6ba36085a78d5ec8070
Leo <factor@mail.ru>
2018-06-07 08:06:15 +0700

    Последние изменения

--- src/main/java/com/ric/bill/excp/WrongGetMethod.java
+++
@@ -1,9 +0,0 @@
-package com.ric.bill.excp;
-
-@SuppressWarnings("serial")
-public class WrongGetMethod extends Exception {
-
-	public WrongGetMethod(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ric/bill/excp/WrongSetMethod.java
+++
@@ -1,9 +0,0 @@
-package com.ric.bill.excp;
-
-@SuppressWarnings("serial")
-public class WrongSetMethod extends Exception {
-
-	public WrongSetMethod(String message) {
-        super(message);
-    }
-}
